0a0fc98d9b9025f2cd87026aec3dd656a92dc5df
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-05-13 12:56:07 +0530

    FileHandling

---
+++ src/filehandling/FileHandlingDemo.java
@@ -0,0 +1,31 @@
+package filehandling;
+
+import java.io.*;
+
+public class FileHandlingDemo {
+    public static void main(String[] args) {
+
+        File file = new File("text.txt");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            String str = "Pune";
+            byte[] arr = str.getBytes();
+            fileOutputStream.write(arr);
+            System.out.println("file write successfully..");
+            fileOutputStream.close();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            int i = fileInputStream.read();
+            while (i>0){
+                System.out.print((char) i);
+                i = fileInputStream.read();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

9f4b60f78775bbc7bd9b5de0eb4aa11710a37d0d
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-05-18 13:00:10 +0530

    pdf generation

---
+++ src/filehandling/PdfGenerate.java
@@ -0,0 +1,31 @@
+package filehandling;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class PdfGenerate {
+    public static void main(String[] args) {
+
+        Document document = new Document();
+        try
+        {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+            document.open();
+            document.add(new Paragraph("A Hello World PDF document."));
+            document.close();
+            writer.close();
+        } catch (DocumentException e)
+        {
+            e.printStackTrace();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+//        https://howtodoinjava.com/java/library/read-generate-pdf-java-itext/
+    }
+}

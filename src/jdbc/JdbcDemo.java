08caf3e57e6d2c523ee95cbff40f093af3e6f090
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-05-23 13:09:03 +0530

    jdbc insert

--- src/jdbc/JdbcDemo.java
+++ src/jdbc/JdbcDemo.java
@@ -3,6 +3,7 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcDemo {
     public static void main(String[] args) {
@@ -10,14 +11,18 @@ public class JdbcDemo {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");//registering driver
 
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java6April","root3","password");
+            Connection connection
+                    = DriverManager.getConnection("jdbc:mysql://localhost:3306/java6April","root3","password");
 
-            System.out.println(connection.isValid(1000));
+            Statement statement = connection.createStatement();
+            String insert = "insert into student(id,name,address) values(2,'Rahul','Mumbai')";
+
+            int i = statement.executeUpdate(insert);
+            System.out.println("record inserted "+i);
 
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
-
 
     }
 }

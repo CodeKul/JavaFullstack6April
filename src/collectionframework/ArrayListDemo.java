9fdf8e32ee6df7fed98728568ff21c310275731d
codekul-vaibhav <codekul.vaibhav@gmail.com>
2022-06-15 12:52:51 +0530

    ArrayListDemo

---
+++ src/collectionframework/ArrayListDemo.java
@@ -0,0 +1,62 @@
+package collectionframework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+//arraylist maintain insertion order
+//arraylist store duplicate values
+//arraylist use dynamic array to store the element
+//it grows by 50%
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+        int[] ints = new int[15];
+        ints[0] = 1;
+        ints[1] = 1;
+        ints[2] = 1;
+
+
+
+        List<Integer>list2 = new ArrayList<>();//10
+        list2.add(10);
+        list2.add(60);
+        list2.add(30);
+
+        list2.add(40);
+
+//        list.addAll(list2);
+//        System.out.println(list.containsAll(list2));
+//        list.retainAll(list2);
+//        System.out.println(list);
+        List<Integer>list = new ArrayList<>();//10
+        list.add(10);
+        list.add(60);
+        list.add(30);
+        list.add(40);
+        System.out.println(list.contains(100));
+        System.out.println(list.get(0));
+        System.out.println(list.indexOf(30));
+        System.out.println(list.isEmpty());
+
+        Iterator<Integer> iterator = list.listIterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+        List<Integer> list1 = new ArrayList<>();
+
+        List<Integer> list3 = Arrays.asList(12,2,3,34,4);
+
+        List<Integer> list4 = new ArrayList<>(Arrays.asList(12,2,3,34,4));
+
+        List<Integer> list5 = new ArrayList<>(List.of(1,2,3,4));
+
+        List<Integer> list6 = new ArrayList<>(){{
+            add(10);add(33);
+        }};
+    }
+}
--- src/collectionframework/PrimeOrNotPrime.java
+++ src/collectionframework/PrimeOrNotPrime.java
@@ -1,28 +1,18 @@
 package collectionframework;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PrimeOrNotPrime {
     public static void main(String[] args) {
 
-        int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9};
-        List<Integer> list = new ArrayList<>();
+        int[] arr = {2, 3, 4, 5, 6, 7, 8, 9};
         for (int j = 0; j < arr.length; j++) {
-            int num = arr[j];
             boolean flag = false;
-            for (int i = 2; i <= num / 2; ++i) {
-                if (num % i == 0) {
+            for (int i = 2; i <= arr[j] / 2; ++i) {
+                if (arr[j] % i == 0) {
                     flag = true;
                     break;
                 }
             }
-            if (!flag)
-                System.out.println(num + " is a prime number.");
-            else
-                System.out.println(num + " is not a prime number.");
+            if (!flag) System.out.println(arr[j]);
         }
-
-        System.out.println(list);
     }
 }
